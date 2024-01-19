e40fde561878ab42b93185a7ab3ce6ea83fe4bb8
Suraj <surajgariya00@gmail.com>
2024-01-19 16:40:36 +0530

    .

---
+++ lib/View/categories.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:meal_app/data/dummy_data.dart';
+import 'package:meal_app/widgets/category_grid_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Meal App'),
+      ),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20),
+        children: [
+          // availableCategories.map((category)=> CategoryGridItem(category: category)).toList()
+          for (final category in availableCategories)
+            CategoryGridItem(category: category)
+        ],
+      ),
+    );
+  }
+}
